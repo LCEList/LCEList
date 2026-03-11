f0ec730d393d1a86b12b4de515b670295f4b2958
SyddersLMAO <sydneyewens06@gmail.com>
2026-03-11 10:31:09 +0000

    addes trusted origins for CSRF

--- lcelist/settings.py
+++ lcelist/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', default='localhost').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'http://localhost').split(',')
 
 # Application definition
 INSTALLED_APPS = [
